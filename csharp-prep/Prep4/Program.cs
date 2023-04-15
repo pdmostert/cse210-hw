ef59f13bc7274b48ef76562e14cfd77ce6f0fb62
peter <peter@DT_PETER>
2023-04-15 13:29:53 +0200

    Prep4 Complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        int newNumber = -1;
+        List<int> numberList = new();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        while (newNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            newNumber = int.Parse(Console.ReadLine());
+
+            //Dont add 0 to the list
+            if (newNumber != 0)
+            {
+                numberList.Add(newNumber);
+
+            }
+        }
+
+        System.Console.WriteLine($"The sum is: {numberList.Sum()}");
+        System.Console.WriteLine($"The average is: {numberList.Average()}");
+        System.Console.WriteLine($"The largest number is: {numberList.Max()}");
+        System.Console.WriteLine($"The smallest positve number is: {numberList.Where(n => n > 0).Min()}");
+        
+        //Sort and Print list items
+        numberList.Sort();
+        foreach (var item in numberList)
+        {
+            System.Console.WriteLine(item);
+        }
     }
 }

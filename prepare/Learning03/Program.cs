f4dad16e59c168cd93d755e70320f09da145e661
peter <peter@DT_PETER>
2023-05-06 10:28:56 +0200

    Fractions completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+
+
+        List<Fraction> fractions = new();
+        Fraction baseFraction = new();
+        Fraction wholeFraction = new(6);
+        Fraction fullFraction = new(6, 7);
+        Fraction f3 = new(3,4);
+        fractions.Add(baseFraction);
+        fractions.Add(wholeFraction);
+        fractions.Add(fullFraction);
+        fractions.Add(f3);
+
+        foreach (var item in fractions)
+        {
+            System.Console.WriteLine(item.GetFractionString());
+            System.Console.WriteLine(item.GetTop());
+            System.Console.WriteLine(item.GetDecimalValue());
+            System.Console.WriteLine();
+        }
+
+
     }
 }
